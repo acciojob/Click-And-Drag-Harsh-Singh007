290535dc45dd7d6f96a1373c7498d638466ce6e7
Harsh Singh <harsh.65004@gmail.com>
2025-06-19 12:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,25 +6,23 @@ let scrollLeft;
 
 scrollContainer.addEventListener('mousedown', (e) => {
   isDragging = true;
-  scrollContainer.classList.add('dragging');
+  scrollContainer.classList.add('active');
   startX = e.pageX - scrollContainer.offsetLeft;
   scrollLeft = scrollContainer.scrollLeft;
 });
 
 scrollContainer.addEventListener('mouseleave', () => {
   isDragging = false;
-  scrollContainer.classList.remove('dragging');
 });
 
 scrollContainer.addEventListener('mouseup', () => {
   isDragging = false;
-  scrollContainer.classList.remove('dragging');
 });
 
 scrollContainer.addEventListener('mousemove', (e) => {
   if (!isDragging) return;
   e.preventDefault();
   const x = e.pageX - scrollContainer.offsetLeft;
-  const walk = (x - startX) * 1.5; // scroll-fast
+  const walk = (x - startX) * 1.5;
   scrollContainer.scrollLeft = scrollLeft - walk;
 });
